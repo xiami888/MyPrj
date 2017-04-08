957fc0ffab982c484aee3a901d1b8a8b9c204688
虾米 <@DESKTOP-C9N8HN5>
2017-04-08 19:44:25 +0800

    测试学习

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我的第二个测试学习");
+        }
+    }
+}
