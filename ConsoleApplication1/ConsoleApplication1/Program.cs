ba2694d37d02ba6bb51d827692518be5991d8db8
虾米 <@DESKTOP-C9N8HN5>
2017-04-08 19:43:09 +0800

    测试

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //测试github
+            //提交测试
             Console.WriteLine("hello word!");
             Console.ReadKey();
         }
