15810db88f9a5eecd7c9bc54b6ee89d0708887b1
虾米 <@DESKTOP-C9N8HN5>
2017-04-08 19:41:48 +0800

    提交

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //测试github
+            Console.WriteLine("hello word!");
+            Console.ReadKey();
+        }
+    }
+}
